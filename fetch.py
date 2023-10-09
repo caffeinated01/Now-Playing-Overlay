d9ab31de2f55e136822f91dfc068796bab12a0f5
Javier <javier.has.no.life@gmail.com>
2023-10-09 18:56:15 +0800

    progress on frontend

--- fetch.py
+++ fetch.py
@@ -11,7 +11,9 @@ SECRET = os.getenv("SECRET") # Get spotify app secret from .env file
 REDIRECT = os.getenv("REDIRECT") # Get spotify app redirect url from .env file
 SCOPE = "user-read-currently-playing" # Initialise scope
 # Get max length of an artist name to display from .env file. If it doesn't exist then defaults to 13 characters
-MAX_ARTIST_LEN = int(os.getenv("MAX_ARTIST_LEN")) if os.getenv("MAX_ARTIST_LEN") and os.getenv("MAX_ARTIST_LEN") != "" else 2
+MAX_ARTIST_LEN = int(os.getenv("MAX_ARTIST_LEN")) if os.getenv("MAX_ARTIST_LEN") and os.getenv("MAX_ARTIST_LEN") != "" else 20
+# Get max length of an song name to display from .env file. If it doesn't exist then defaults to 39 characters
+MAX_NAME_LEN = int(os.getenv("MAX_NAME_LEN")) if os.getenv("MAX_NAME_LEN") and os.getenv("MAX_NAME_LEN") != "" else 39
 
 # Initialise oauth
 oauth = spotipy.SpotifyOAuth(client_id=ID,
@@ -32,8 +34,8 @@ def fetch():
     # Only returns song details if a - spotify is open on client side and b - song is not paused
     if current != None and current['is_playing'] == True:
         song_id = current['item']['id']
-        song_name = current['item']['name']
-        song_cover = current['item']['album']['images'][1]['url']
+        song_name = current['item']['name'][:MAX_NAME_LEN] + "..." if len(current['item']['name']) > MAX_NAME_LEN else current['item']['name']
+        song_cover = current['item']['album']['images'][0]['url']
         artists = [artist for artist in current['item']['artists']]
         # Join all the artist names into one string and slice up to 13 characters and ellipsis so artist name does not overflow
         artist_names = artist_names = ', '.join([artist['name'] for artist in artists])[:MAX_ARTIST_LEN] + "..." if len(', '.join([artist['name'] for artist in artists])) > MAX_ARTIST_LEN else ', '.join([artist['name'] for artist in artists])
